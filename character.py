9579f9fa6cd2e1cfe48cf562616441b6bedb82d9
Sanjeet Mistry <sanjeetmistry10@gmail.com>
2026-03-09 22:51:52 +0530

    Update character personality

--- character.py
+++ character.py
@@ -39,7 +39,7 @@ class Character():
             "height": "177 cm",
             "weight": "78 kg",
             "gender": gender[1],
-            "personality": "queen",
+            "personality": "confident, loving & adventurous",
             "occupation": "Instagram Model",
             "ethinicity": ethinicity[2],
             "figure": "curvy",
