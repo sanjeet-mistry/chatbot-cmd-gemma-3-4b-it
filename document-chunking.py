d217a3b3dc1651d9dcb0f62c3d2ef521399b3332
Sanjeet Mistry <sanjeetmistry10@gmail.com>
2026-04-22 12:15:33 +0530

    Import stuff where needed. Make code efficient.

--- document-chunking.py
+++ document-chunking.py
@@ -1,11 +1,6 @@
-from embeddings import return_similarity_scores, calculate_embeddings
-from chat import Chat
-from data import Data
-from assistant import Assistant
-
-assistant1 = Assistant(Data.assistants[3], Data.user_info)
-chat1 = Chat(Data.user_info,
-             assistant1, "assistant", Data.assistant_chat_params, 0)
+calculate_embedding = False
+calculate_similarity = True
+file_name = "sherlock-holmes-e5-base-v2"
 
 
 def chunk_text(text, chunk_size=200):
@@ -23,18 +18,35 @@ with open("./week-3/chatbot-cmd-class/data/sherlock-holmes.txt", encoding='utf-8
     text = f.read()
 chunks = chunk_text(text)
 queries = [
-    "Who is referred to as “the woman” by Sherlock Holmes?",
     "What is the name of Holmes’s close friend and narrator of the story?",
-    "Why had Holmes and Watson seen little of each other lately?",
     "What street does Holmes live on?",
-    "What habit of Holmes is mentioned involving cocaine and ambition?"
+    "How many steps lead up to Holmes’s room?",
+    "How does Holmes deduce that Watson had been out in bad weather?",
+    "At what time is the mysterious visitor supposed to arrive?",
+    "From which region does Holmes deduce the paper of the letter comes?",
+    "What disguise does Holmes use to investigate Irene Adler?",
+    "What does the King want Holmes to recover?",
+    "What trick does Holmes use to find where the photograph is hidden?",
+    "Why does Holmes ultimately fail to obtain the photograph?"
 ]
-# query = "What habit of Holmes is mentioned involving cocaine and ambition?"
-# chunks_embeddings = calculate_embeddings("array", chunks, "sherlock-holmes-2")
-for query in queries:
-    similarity = return_similarity_scores(
-        chunks, "json", "./week-3/chatbot-cmd-class/data/sherlock-holmes-2.json", query, 3)
-    # print(similarity_texts)
-    print(query)
-    answer = chat1.generate_output(query, similarity)
-    print(f"{answer}\n")
+if calculate_embedding:
+    from embeddings import calculate_embeddings
+    chunks_embeddings = calculate_embeddings(
+        "array", chunks, file_name)
+elif calculate_similarity:
+    from embeddings import return_similarity_scores
+    similarity_scores = []
+    for query in queries:
+        similarity_scores.append(return_similarity_scores(
+            chunks, "json", f"./week-3/chatbot-cmd-class/data/{file_name}.json", query, 4))
+
+    from data import Data
+    from assistant import Assistant
+    assistant1 = Assistant(Data.assistants[3], Data.user_info)
+    from chat import Chat
+    chat1 = Chat(Data.user_info,
+                 assistant1, "assistant", Data.assistant_chat_params, 0)
+    for i in range(len(similarity_scores)):
+        print(queries[i])
+        answer = chat1.generate_output(queries[i], similarity_scores[i])
+        print(f"{answer}\n")
