f7922db426a837b43f120b314633d53a8da3f1e0
Sanjeet Mistry <sanjeetmistry10@gmail.com>
2026-05-19 19:29:42 +0530

    Extract chapter title, page text from every page of a pdf book using regex

---
+++ chroma-vector-db-harry-potter.py
@@ -0,0 +1,62 @@
+import fitz  # PyMuPDF
+from core.chat import Chat
+from core.data import Data
+from core.utils import chunk_text_overlap
+from core.chroma_vector_db import ChromaVectorDB
+import json
+
+chunks_setting = Data.chunks[0]
+file_name = "harry-potter-and-the-sorcerer-stone"
+file_path = f"./week-3/chatbot-cmd-class/data/{file_name}"
+file_type = "pdf"
+collection_file_name = f"{file_name}-{chunks_setting['size']}-{chunks_setting['overlap']}"
+collection_file_path = "./week-3/chatbot-cmd-class/chroma-db/"
+
+document_metadata = {
+    "source": "",
+    "title": "",
+    "series": "Harry Potter",
+    "book_number": 1,
+    "author": "J. K. Rowling",
+    "category": ["fantasy", "fiction", "young-adult"],
+    "language": "en",
+    "publication_year": 0,
+    "universe": "Wizarding World"
+}
+
+with open(f"./week-3/chatbot-cmd-class/queries/{file_name}.txt", encoding="utf-8") as file:
+    text = file.read()
+    questions = text.split("\n\n")
+
+chromaVectorDB = ChromaVectorDB()
+createCollection = False
+
+if createCollection:
+    if file_type == "pdf":
+        doc = fitz.open(f"{file_path}.pdf")
+        all_text = ""
+        for page in doc:
+            text = page.get_text()
+            all_text += text
+    elif file_type == "text":
+        with open(f"{file_path}.txt", encoding="utf-8") as f:
+            all_text = f.read()
+
+    chunks = chunk_text_overlap(all_text, chunks_setting["size"],
+                                chunks_setting["overlap"])
+
+    with open(f"./week-3/chatbot-cmd-class/generated/{collection_file_name}.json") as file:
+        embeddings_array = json.load(file)["embeddings"]
+
+    chromaVectorDB.create_collection(
+        chunks, embeddings_array, collection_file_path, collection_file_name)
+
+else:
+    results = chromaVectorDB.return_best_results(
+        collection_file_name, collection_file_path, questions, 4, True, {'min': 2, 'max': 8})
+    chat1 = Chat("query", Data.user_info, None, Data.assistant_chat_params, 0)
+
+    for question, result in zip(questions, results):
+        print(f"Question: {question}")
+        response = chat1.generate_output(question, result)
+        print(f"Answer:\n{response}\n")
---
+++ regular-expression.py
@@ -0,0 +1,112 @@
+import fitz
+import re
+
+file_name = "./week-3/chatbot-cmd-class/data/harry-potter-and-the-prisoner-of-azkaban.pdf"
+
+current_chapter = None
+chapter_start_pattern = r"C H A P T E R[\s]+[A-Z]+"
+
+
+def extract_and_clean_page_content(page_blocks):
+    global current_chapter
+    length = len(page_blocks)
+    obj = {}
+    if length:
+        first_sentence = page_blocks[0]
+        match = re.match(chapter_start_pattern, first_sentence)
+        # new chapter start page
+        if match:
+            if current_chapter is None:
+                current_chapter = 1
+            elif isinstance(current_chapter, int):
+                current_chapter += 1
+
+            if length == 4:
+                # extract chapter info
+                book_page_number = page_blocks[3]
+                if (re.search(r"\d+", book_page_number)):
+                    book_page_number = int(
+                        re.search(r"\d+", book_page_number).group())
+
+                text = page_blocks[2].replace("-\n", "")
+                match = re.search(r"([A-Z\-\']+[\s]+)+", text)
+                if match:
+                    chapter_title = match.group().replace("\n", "").strip()
+                text = re.sub(r"([A-Z\-]+[\s]+)+", "", text)
+                chapter_text = text.strip()
+
+                obj = {
+                    "chapter-title": chapter_title,
+                    "chapter-number": current_chapter,
+                    "chapter-text": chapter_text,
+                    "book-page-number": book_page_number
+                }
+            else:
+                # extract chapter info
+                chapter_title = page_blocks[1]
+                chapter_text_on_page = ""
+                for i in range(2, length-1):
+                    if i <= 3:
+                        chapter_text_on_page += page_blocks[i]
+                    else:
+                        chapter_text_on_page += "\n" + page_blocks[i]
+
+                book_page_number = page_blocks[length - 1]
+
+                # clean info
+                chapter_title = chapter_title.replace("\n", "")
+                chapter_text_on_page = chapter_text_on_page.replace(
+                    "-\n", "")
+                if (re.search(r"\d+", book_page_number)):
+                    book_page_number = int(
+                        re.search(r"\d+", book_page_number).group())
+
+                obj = {
+                    "chapter-title": chapter_title,
+                    "chapter-number": current_chapter,
+                    "chapter-text": chapter_text_on_page,
+                    "book-page-number": book_page_number
+                }
+            return obj
+        # chapter page
+        elif current_chapter:
+            # extract chapter info
+            chapter_text = ""
+            for i in range(1, length - 1):
+                match = re.search(r"\x91", page_blocks[i])
+                if match is None:
+                    chapter_text += page_blocks[i] + "\n"
+            book_page_number = page_blocks[length - 1]
+
+            # clean info
+            chapter_text = chapter_text.replace("-\n", "")
+            if (re.search(r"\d+", book_page_number)):
+                book_page_number = int(
+                    re.search(r"\d+", book_page_number).group())
+            else:
+                return None
+            obj = {
+                "chapter-text": chapter_text,
+                "chapter-number": current_chapter,
+                "book-page-number": book_page_number
+            }
+            return obj
+    return None
+
+
+pages = []
+
+with fitz.open(file_name) as doc:
+    for index, page in enumerate(doc, start=1):
+        blocks = page.get_text_blocks(sort=True)
+        if len(blocks) > 0:
+            text_array = [block[4].strip() for block in blocks]
+            text_array = [text for text in text_array if text]
+            # if index == 305:
+            #     print(len(text_array))
+            #     print(text_array)
+            obj = extract_and_clean_page_content(text_array)
+            if obj:
+                # print(f"{obj['chapter-number']}, {obj['book-page-number']}")
+                pages.append(obj)
+    print(len(pages))
